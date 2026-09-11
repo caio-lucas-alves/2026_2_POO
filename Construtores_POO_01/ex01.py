bc22d3c12bce679af48f87ec306d6badc5c6a955
caio-lucas-alves <oliveira.caio1@escolar.ifrn.edu.br>
2026-09-11 12:59:29 +0000

    Construtores_POO_01

--- Construtores_POO_01/ex01.py
+++ Construtores_POO_01/ex01.py
@@ -1,5 +1,39 @@
 class retangulo:
     def __init__(self, b, h):
-        self.b = b
-        self.h = h
+        if b <= 0:
+            print("A base deve ser positiva")
+
+        if h <= 0:
+            print("A altura deve ser positiva")
+
+        else:
+            self.b = b
+            self.h = h
+
+    def SetBase(self, nova_base):
+        if nova_base <= 0:
+            print ("A nova base precisa ser positiva")
+
+        self.b = nova_base
+
+    def SetAltura(self, nova_altura):
+        if nova_altura <= 0:
+            print ("A altura precisa ser positiva")
+
+        self.a = nova_altura
+
+    def GetBase(self):
+        return self.b
         
+    def GetAltura(self):
+        return self.h
+
+    def CalcArea(self):
+        return self.b * self.h
+
+    def CalcDiagonal(self):
+        return math.sqrt(self.b**2 + self.h**2)
+
+    def ToString(self):
+        return f"Base: {self.b}, Altura: {self.h}"
+
